8adf9ef53fa032b0dbe960171ea1236c0aa63ec5
hopprAbhinavPandey <58676009+abhinav-pandey29@users.noreply.github.com>
2022-10-31 04:45:24 +1100

    refactor(vision): allow only one command to be activated at a time

--- vision.py
+++ vision.py
@@ -1,11 +1,10 @@
 import time
-from typing import List
+from typing import List, Optional
 
 import cv2
 
 from gesture.commands import (
     GestureCommand,
-    shuffle_saved_tracks_command_factory,
     play_next_track_command_factory,
     play_prev_track_command_factory,
     set_volume_command_factory,
@@ -27,42 +26,38 @@ class Vision:
         self.hand_detector = get_hand_detector(draw=draw)
         self.gesture_commands = gestures if gestures is not None else []
 
-    def get_activated_commands(self, g: List[Finger]) -> List[GestureCommand]:
-        return [
-            command for command in self.gesture_commands if command.gesture.match(g)
-        ]
+    def get_activated_command(self, g: List[Finger]) -> Optional[List[GestureCommand]]:
+        for command in self.gesture_commands:
+            if command.gesture.match(g):
+                return command
+        return None
 
     def activate(self, camera_id: int = 0):
         cap = cv2.VideoCapture(camera_id)
         if not cap.isOpened():
             raise IOError("Cannot open webcam :(")
 
-        command_queue = []
         while cap.isOpened():
-            if not command_queue:
+            activated_command = None
                 ret, frame = cap.read()
                 image = cv2.flip(frame, 1)  # Flip on horizontal axis
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                 found_hands = self.hand_detector.find_hands(image)
                 if found_hands:
                     fingers = [
-                        k
-                        for hand in found_hands
-                        for k, v in hand["fingers"].items()
-                        if v
+                    k for hand in found_hands for k, v in hand["fingers"].items() if v
                     ]
-                    command_queue.extend(self.get_activated_commands(fingers))
-            else:
-                command = command_queue.pop()
-                result = command(
+
+                activated_command = self.get_activated_command(fingers)
+                if activated_command:
+                    result = activated_command(
                     hand_detector=self.hand_detector,
                     image=image,
                     found_hands=found_hands,
                     draw=self._draw,
                 )
                 cv2.imshow("Hand Tracking", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-                cv2.waitKey(10)
-                time.sleep(2)
+                    cv2.waitKey(1)
 
             cv2.imshow("Hand Tracking", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
             if cv2.waitKey(10) & 0xFF == ord("q"):
