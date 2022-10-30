c01ec746f3ff7fb7532bc47b415a2e2cae1798c5
hopprAbhinavPandey <58676009+abhinav-pandey29@users.noreply.github.com>
2022-10-31 04:48:17 +1100

    fix(vision): fix faulty whitespaces in code

--- vision.py
+++ vision.py
@@ -39,24 +39,24 @@ class Vision:
 
         while cap.isOpened():
             activated_command = None
-                ret, frame = cap.read()
-                image = cv2.flip(frame, 1)  # Flip on horizontal axis
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                found_hands = self.hand_detector.find_hands(image)
-                if found_hands:
-                    fingers = [
+            ret, frame = cap.read()
+            image = cv2.flip(frame, 1)  # Flip on horizontal axis
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            found_hands = self.hand_detector.find_hands(image)
+            if found_hands:
+                fingers = [
                     k for hand in found_hands for k, v in hand["fingers"].items() if v
-                    ]
+                ]
 
                 activated_command = self.get_activated_command(fingers)
                 if activated_command:
                     result = activated_command(
-                    hand_detector=self.hand_detector,
-                    image=image,
-                    found_hands=found_hands,
-                    draw=self._draw,
-                )
-                cv2.imshow("Hand Tracking", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+                        hand_detector=self.hand_detector,
+                        image=image,
+                        found_hands=found_hands,
+                        draw=self._draw,
+                    )
+                    cv2.imshow("Hand Tracking", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
                     cv2.waitKey(1)
 
             cv2.imshow("Hand Tracking", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
