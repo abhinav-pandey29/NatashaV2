cfdfedfe7461f66ebfb601d2726ff082c48531bf
abhinav-pandey29 <58676009+abhinav-pandey29@users.noreply.github.com>
2024-01-13 12:42:25 +0800

    refactor(hands): minor updates to simplify HandDetector classes

--- src/vision/hands.py
+++ src/vision/hands.py
@@ -5,9 +5,8 @@ import cv2
 import numpy as np
 from mediapipe.framework.formats.landmark_pb2 import NormalizedLandmarkList
 from mediapipe.python.solutions import drawing_utils
-from mediapipe.python.solutions.hands import HAND_CONNECTIONS
+from mediapipe.python.solutions.hands import HAND_CONNECTIONS, Hands
 from mediapipe.python.solutions.hands import HandLandmark as HL
-from mediapipe.python.solutions.hands import Hands
 
 from config.settings import settings
 
@@ -15,7 +14,6 @@ POINT_2D = Tuple[float, float]
 
 
 class Finger(str, Enum):
-
     LEFT_THUMB = "LEFT_THUMB"
     LEFT_INDEX_FINGER = "LEFT_INDEX"
     LEFT_MIDDLE_FINGER = "LEFT_MIDDLE"
@@ -31,13 +29,14 @@ class Finger(str, Enum):
 class HandDetector:
     """Wrapper for Hand Detector service."""
 
-    detector = Hands(
-        static_image_mode=False,
-        max_num_hands=2,
-        model_complexity=1,
-        min_detection_confidence=0.8,
-        min_tracking_confidence=0.6,
-    )
+    def __init__(self):
+        self.detector = Hands(
+            static_image_mode=False,
+            max_num_hands=2,
+            model_complexity=1,
+            min_detection_confidence=0.8,
+            min_tracking_confidence=0.6,
+        )
 
     def find_hands(self, image_rgb: np.ndarray) -> List[Dict[str, Any]]:
         """Returns array of hands from an RGB image."""
@@ -94,8 +93,10 @@ class HandDetector:
             - _PIP: Proximal interphalangeal joint
             - _MCP: Metacarpophalangeal joint.
         """
+
         def get_x(lmark):
             return hand_landmarks.landmark[lmark].x
+
         def get_y(lmark):
             return hand_landmarks.landmark[lmark].y
 
@@ -122,82 +123,43 @@ class HandDetector:
 
         prefix_ = hand_label.upper() + "_"
         return {
-            f"{prefix_}THUMB": is_thumb_up,
-            f"{prefix_}INDEX": is_index_up,
-            f"{prefix_}MIDDLE": is_middle_up,
-            f"{prefix_}RING": is_ring_up,
-            f"{prefix_}PINKY": is_pinky_up,
+            Finger(f"{prefix_}THUMB"): is_thumb_up,
+            Finger(f"{prefix_}INDEX"): is_index_up,
+            Finger(f"{prefix_}MIDDLE"): is_middle_up,
+            Finger(f"{prefix_}RING"): is_ring_up,
+            Finger(f"{prefix_}PINKY"): is_pinky_up,
         }
 
 
 class HandDetectorArtist(HandDetector):
     """Hand detector service with additional drawing features"""
 
-    def __init__(
-        self,
-        font_type=None,
-        text_color=None,
-        line_type=None,
-        joint_drawing_spec=None,
-        connection_drawing_spec=None,
-        bbox_padding=None,
-    ):
-        self._font_type = font_type
-        self._text_color = text_color
-        self._line_type = line_type
-        self._joint_drawing_spec = joint_drawing_spec
-        self._connection_drawing_spec = connection_drawing_spec
-        self._bbox_padding = bbox_padding
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.artist_settings = {
+            "font_type": settings.CV2_FONT_TYPE,
+            "text_color": settings.CV2_TEXT_COLOR,
+            "line_type": settings.CV2_LINE_TYPE,
+            "joint_drawing_spec": settings.HAND_JOINT_SPEC,
+            "connection_drawing_spec": settings.HAND_CONNECTION_SPEC,
+            "bbox_padding": settings.BBOX_PADDING,
+            **kwargs,
+        }
 
-    @property
-    def font_type(self):
-        return self._font_type or settings.CV2_FONT_TYPE
-
-    @property
-    def text_color(self):
-        return self._text_color or settings.CV2_TEXT_COLOR
-
-    @property
-    def line_type(self):
-        return self._line_type or settings.CV2_LINE_TYPE
-
-    @property
-    def joint_drawing_spec(self):
-        return self._joint_drawing_spec or settings.HAND_JOINT_SPEC
-
-    @property
-    def connection_drawing_spec(self):
-        return self._connection_drawing_spec or settings.HAND_CONNECTION_SPEC
-
-    @property
-    def bbox_padding(self):
-        return self._bbox_padding or settings.BBOX_PADDING
-
-    def set_font_type(self, font_type):
-        self._font_type = font_type
-
-    def set_text_color(self, text_color):
-        self._text_color = text_color
-
-    def set_line_type(self, line_type):
-        self._line_type = line_type
-
-    def set_joint_drawing_spec(self, spec):
-        self._joint_drawing_spec = spec
-
-    def set_connection_drawing_spec(self, spec):
-        self._connection_drawing_spec = spec
-
-    def set_bbox_padding(self, value):
-        self._bbox_padding = value
+    def update_artist_settings(self, **kwargs):
+        """Update drawing settings."""
+        self.artist_settings.update(kwargs)
 
     def reset_artist(self):
-        self.set_font_type(None)
-        self.set_text_color(None)
-        self.set_line_type(None)
-        self.set_joint_drawing_spec(None)
-        self.set_connection_drawing_spec(None)
-        self.set_bbox_padding(None)
+        """Reset drawing settings to default."""
+        self.update_artist_settings(
+            font_type=settings.CV2_FONT_TYPE,
+            text_color=settings.CV2_TEXT_COLOR,
+            line_type=settings.CV2_LINE_TYPE,
+            joint_drawing_spec=settings.HAND_JOINT_SPEC,
+            connection_drawing_spec=settings.HAND_CONNECTION_SPEC,
+            bbox_padding=settings.BBOX_PADDING,
+        )
 
     def find_hands(self, image_rgb: np.ndarray) -> List[Dict[str, Any]]:
         """
@@ -213,8 +175,8 @@ class HandDetectorArtist(HandDetector):
                 image_rgb,
                 hand["landmarks"],
                 HAND_CONNECTIONS,
-                self.joint_drawing_spec,
-                self.connection_drawing_spec,
+                self.artist_settings["joint_drawing_spec"],
+                self.artist_settings["connection_drawing_spec"],
             )
             # Draw hand label (Left, Right) and score
             label_target = hand["landmarks"].landmark[HL.WRIST]
@@ -224,16 +186,16 @@ class HandDetectorArtist(HandDetector):
                 image_rgb,
                 f"{hand['label']} ({hand['score']:.2f})",
                 label_pos,
-                self.font_type,
+                self.artist_settings["font_type"],
                 0.5,
-                self.text_color,
+                self.artist_settings["text_color"],
                 1,
-                self.line_type,
+                self.artist_settings["line_type"],
             )
             # Draw bounding box
             pt1, pt2 = hand["bbox"]
-            pt1 = tuple(int(x - self.bbox_padding) for x in pt1)
-            pt2 = tuple(int(x + self.bbox_padding) for x in pt2)
+            pt1 = tuple(int(x - self.artist_settings["bbox_padding"]) for x in pt1)
+            pt2 = tuple(int(x + self.artist_settings["bbox_padding"]) for x in pt2)
             cv2.rectangle(image_rgb, pt1, pt2, (200, 123, 90), 2)
             # Draw finger labels
             gesture = [k for k, v in hand["fingers"].items() if v]
@@ -241,7 +203,7 @@ class HandDetectorArtist(HandDetector):
                 line = f"{hand['label']}: {' '.join(gesture)}"
                 (w, h), _ = cv2.getTextSize(
                     text=line,
-                    fontFace=self.font_type,
+                    fontFace=self.artist_settings["font_type"],
                     fontScale=0.5,
                     thickness=1,
                 )
@@ -251,11 +213,11 @@ class HandDetectorArtist(HandDetector):
                     image_rgb,
                     text=line,
                     org=org,
-                    fontFace=self.font_type,
+                    fontFace=self.artist_settings["font_type"],
                     fontScale=0.35,
-                    color=self.text_color,
+                    color=self.artist_settings["text_color"],
                     thickness=1,
-                    lineType=self.line_type,
+                    lineType=self.artist_settings["line_type"],
                 )
                 uv_top_left += [0, h * line_spacing]
         # Draw finger count
@@ -265,11 +227,11 @@ class HandDetectorArtist(HandDetector):
             image_rgb,
             f"Total Finger: {total_fingers}",
             label_pos,
-            self.font_type,
-            0.5,
-            self.text_color,
-            1,
-            self.line_type,
+            fontFace=self.artist_settings["font_type"],
+            fontScale=0.5,
+            color=self.artist_settings["text_color"],
+            thickness=1,
+            lineType=self.artist_settings["line_type"],
         )
 
         return found_hands
